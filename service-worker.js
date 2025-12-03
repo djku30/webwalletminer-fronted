29ba96cc3f954af5c0957c270c9b6e4dd42e654a
djku30 <djku300193@gmail.com>
2025-12-03 02:50:17 -0600

    Updates

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+// Basic service worker for PWA offline support
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
